10c502a921ced5dd20f4404187982f08cf960c0d
Bradley Davis <me@bradleydavis.tech>
2023-07-10 02:04:53 -0700

    Added portfolio.get_value_by_category & asset

--- nummus/web/controller_portfolio.py
+++ nummus/web/controller_portfolio.py
@@ -8,7 +8,7 @@ import datetime
 import flask
 
 from nummus import portfolio
-from nummus.models import Account, AccountCategory
+from nummus.models import Account, AccountCategory, Asset, AssetCategory
 from nummus.web import common
 from nummus.web.common import HTTPError
 
@@ -47,6 +47,7 @@ def get_value() -> flask.Response:
     liabilities: t.List[float] = [0] * len(dates)
 
     for acct in query.all():
+      acct: Account
       _, a_values, _ = acct.get_value(start, end)
       for i, v in enumerate(a_values):
         total[i] += v
@@ -97,10 +98,106 @@ def get_value_by_account() -> flask.Response:
     accounts: t.Dict[str, t.List[float]] = {}
 
     for acct in query.all():
+      acct: Account
       _, a_values, _ = acct.get_value(start, end)
       for i, v in enumerate(a_values):
         total[i] += v
       accounts[acct.uuid] = a_values
 
     response = {"total": total, "accounts": accounts, "dates": dates}
+    return flask.jsonify(response)
+
+
+def get_value_by_category() -> flask.Response:
+  """GET /api/portfolio/value-by-category
+
+  Returns:
+    JSON response, see api.yaml for details
+  """
+  with flask.current_app.app_context():
+    p: portfolio.Portfolio = flask.current_app.portfolio
+  today = datetime.date.today()
+
+  args: t.Dict[str, object] = flask.request.args.to_dict()
+  start = common.parse_date(args.get("start", today))
+  end = common.parse_date(args.get("end", today))
+  if end < start:
+    raise HTTPError(422, detail="End date must be on or after Start date")
+
+  with p.get_session() as s:
+    query = s.query(Account)
+
+    # Prepare dates
+    dates: t.List[datetime.date] = []
+    date = start
+    while date <= end:
+      dates.append(date)
+      date += datetime.timedelta(days=1)
+
+    total: t.List[float] = [0] * len(dates)
+    categories: t.Dict[str, t.List[float]] = {
+        k.name.lower(): [0] * len(dates) for k in AccountCategory
+    }
+
+    for acct in query.all():
+      acct: Account
+      _, a_values, _ = acct.get_value(start, end)
+      cat_values = categories[acct.category.name.lower()]
+      for i, v in enumerate(a_values):
+        total[i] += v
+        cat_values[i] += v
+
+    response = {"total": total, "categories": categories, "dates": dates}
+    return flask.jsonify(response)
+
+
+def get_value_by_asset() -> flask.Response:
+  """GET /api/portfolio/value-by-asset
+
+  Returns:
+    JSON response, see api.yaml for details
+  """
+  with flask.current_app.app_context():
+    p: portfolio.Portfolio = flask.current_app.portfolio
+  today = datetime.date.today()
+
+  args: t.Dict[str, object] = flask.request.args.to_dict()
+  start = common.parse_date(args.get("start", today))
+  end = common.parse_date(args.get("end", today))
+  category = common.parse_enum(args.get("category"), AssetCategory)
+  if end < start:
+    raise HTTPError(422, detail="End date must be on or after Start date")
+
+  with p.get_session() as s:
+    q_accts = s.query(Account)
+
+    q_assets = s.query(Asset.uuid)
+    if category is not None:
+      q_assets = q_assets.where(Asset.category == category)
+    allowed_assets: t.List[str] = [uuid for uuid, in q_assets.all()]
+
+    # Prepare dates
+    dates: t.List[datetime.date] = []
+    date = start
+    while date <= end:
+      dates.append(date)
+      date += datetime.timedelta(days=1)
+
+    assets: t.Dict[str, t.List[float]] = {}
+
+    for acct in q_accts.all():
+      acct: Account
+      _, _, acct_assets = acct.get_value(start, end)
+      for a, a_values in acct_assets.items():
+        if a not in allowed_assets:
+          continue
+        if a not in assets:
+          assets[a] = a_values
+        else:
+          # Add on to existing
+          a_values_sum = assets[a]
+          for i, v in enumerate(a_values):
+            a_values_sum[i] += v
+
+    response = {"assets": assets, "dates": dates}
     return flask.jsonify(response)
--- tests/web/test_controller_portfolio.py
+++ tests/web/test_controller_portfolio.py
@@ -29,11 +29,15 @@ class TestControllerPortfolio(WebTestBase):
       acct_invest = Account(name="Monkey Investments",
                             institution="Monkey Bank",
                             category=AccountCategory.INVESTMENT)
-      asset = Asset(name="BANANA", category=AssetCategory.SECURITY)
-      s.add_all((acct_checking, acct_savings, acct_invest, asset))
+      a_banana = Asset(name="BANANA", category=AssetCategory.SECURITY)
+      a_apple = Asset(name="Apple", category=AssetCategory.ITEM)
+      s.add_all((acct_checking, acct_savings, acct_invest, a_banana, a_apple))
       s.commit()
 
-      v = AssetValuation(asset=asset, date=today, value=1)
+      v = AssetValuation(asset=a_banana, date=today, value=1)
+      s.add(v)
+
+      v = AssetValuation(asset=a_apple, date=today, value=1)
       s.add(v)
 
       for _ in range(n_transactions):
@@ -49,19 +53,30 @@ class TestControllerPortfolio(WebTestBase):
                           date=today,
                           total=self._RNG.uniform(-10, 10),
                           statement=self.random_string())
-        t_split = TransactionSplit(total=txn.total, parent=txn)
-        s.add_all((txn, t_split))
+        t_split_0 = TransactionSplit(total=txn.total, parent=txn)
+        t_split_1 = TransactionSplit(total=txn.total,
+                                     parent=txn,
+                                     asset=a_apple,
+                                     asset_quantity=self._RNG.uniform(
+                                         0.001, 0.01))
+        s.add_all((txn, t_split_0, t_split_1))
 
       for _ in range(n_transactions):
         txn = Transaction(account=acct_invest,
                           date=today,
                           total=self._RNG.uniform(-10, -1),
                           statement=self.random_string())
-        t_split = TransactionSplit(total=txn.total,
-                                   parent=txn,
-                                   asset=asset,
-                                   asset_quantity=self._RNG.uniform(100, 1000))
-        s.add_all((txn, t_split))
+        t_split_0 = TransactionSplit(total=txn.total,
+                                     parent=txn,
+                                     asset=a_banana,
+                                     asset_quantity=self._RNG.uniform(
+                                         100, 1000))
+        t_split_1 = TransactionSplit(total=txn.total,
+                                     parent=txn,
+                                     asset=a_apple,
+                                     asset_quantity=self._RNG.uniform(
+                                         100, 1000))
+        s.add_all((txn, t_split_0, t_split_1))
       s.commit()
 
   def test_get_value(self):
@@ -202,3 +217,129 @@ class TestControllerPortfolio(WebTestBase):
         "dates": [today.isoformat()]
     }
     self.assertEqualWithinError(target, result, 1e-6)
+
+  def test_get_value_by_category(self):
+    p = self._portfolio
+    self.prepare_portfolio()
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+
+    with p.get_session() as s:
+      total = 0
+      categories = {k.name.lower(): 0 for k in AccountCategory}
+
+      q = s.query(Account).where(Account.category == AccountCategory.CASH)
+      for acct in q.all():
+        _, values, _ = acct.get_value(today, today)
+        value = values[0]
+        categories["cash"] += value
+        total += value
+
+      q = s.query(Account).where(Account.category == AccountCategory.INVESTMENT)
+      for acct in q.all():
+        _, values, _ = acct.get_value(today, today)
+        value = values[0]
+        categories["investment"] += value
+        total += value
+
+    endpoint = "/api/portfolio/value-by-category"
+
+    result, _ = self.api_get(endpoint)
+    target = {
+        "total": [total],
+        "categories": {
+            k: [v] for k, v in categories.items()
+        },
+        "dates": [today.isoformat()]
+    }
+    self.assertEqualWithinError(target, result, 1e-6)
+
+    result, _ = self.api_get(endpoint, {"start": yesterday, "end": today})
+    target = {
+        "total": [0, total],
+        "categories": {
+            k: [0, v] for k, v in categories.items()
+        },
+        "dates": [yesterday.isoformat(),
+                  today.isoformat()]
+    }
+    self.assertEqualWithinError(target, result, 1e-6)
+
+    # Invalid date filters
+    self.api_get(endpoint, {"start": today, "end": yesterday}, rc=422)
+
+    # Invalid date format
+    self.api_get(endpoint, {"start": self.random_string()}, rc=400)
+
+  def test_get_value_by_asset(self):
+    p = self._portfolio
+    self.prepare_portfolio()
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+
+    # All assets
+    with p.get_session() as s:
+      assets = {}
+
+      q = s.query(Account)
+      for acct in q.all():
+        _, _, acct_assets = acct.get_value(today, today)
+        for a, a_values in acct_assets.items():
+          if a not in assets:
+            assets[a] = a_values[0]
+          else:
+            assets[a] += a_values[0]
+
+    endpoint = "/api/portfolio/value-by-asset"
+
+    result, _ = self.api_get(endpoint)
+    target = {
+        "assets": {
+            k: [v] for k, v in assets.items()
+        },
+        "dates": [today.isoformat()]
+    }
+    self.assertEqualWithinError(target, result, 1e-6)
+
+    result, _ = self.api_get(endpoint, {"start": yesterday, "end": today})
+    target = {
+        "assets": {
+            k: [0, v] for k, v in assets.items()
+        },
+        "dates": [yesterday.isoformat(),
+                  today.isoformat()]
+    }
+    self.assertEqualWithinError(target, result, 1e-6)
+
+    # Only SECURITIES
+    with p.get_session() as s:
+      assets = {}
+
+      q = s.query(Account)
+      for acct in q.all():
+        _, _, acct_assets = acct.get_value(today, today)
+        for a, a_values in acct_assets.items():
+          asset = s.query(Asset).where(Asset.uuid == a).first()
+          if asset.category != AssetCategory.SECURITY:
+            continue
+          if a not in assets:
+            assets[a] = a_values[0]
+          else:
+            assets[a] += a_values[0]
+
+    endpoint = "/api/portfolio/value-by-asset"
+
+    result, _ = self.api_get(endpoint, {"category": "security"})
+    target = {
+        "assets": {
+            k: [v] for k, v in assets.items()
+        },
+        "dates": [today.isoformat()]
+    }
+    self.assertEqualWithinError(target, result, 1e-6)
+
+    # Invalid date filters
+    self.api_get(endpoint, {"start": today, "end": yesterday}, rc=422)
+
+    # Invalid date format
+    self.api_get(endpoint, {"start": self.random_string()}, rc=400)
