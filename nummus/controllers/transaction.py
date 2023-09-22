23f83606e853be25ec48053a76baf26306a94b93
Bradley Davis <me@bradleydavis.tech>
2023-09-21 20:35:26 -0700

    Created filter dropdown HTML

--- nummus/controllers/transaction.py
+++ nummus/controllers/transaction.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 
 import flask
 import sqlalchemy
+from sqlalchemy import orm
 
 from nummus import portfolio, web_utils
 from nummus import custom_types as t
@@ -33,6 +34,79 @@ def get_body() -> str:
     string HTML response
   """
   return flask.render_template("transactions/body.html", table=ctx_table())
+
+
+def ctx_options_account(query: orm.Query,
+                        accounts: t.DictIntStr) -> t.List[t.DictStr]:
+  """Get the context to build the options for account
+
+  Args:
+    s: Session to use
+    query: Query to use to get distinct values
+    accounts: Account ID to name mapping
+
+  Returns:
+    List of HTML context
+  """
+  # Get options with these filters
+  options: t.List[t.DictStr] = []
+  for acct_id, in query.with_entities(TransactionSplit.account_id).distinct():
+    options.append({"name": accounts[acct_id]})
+  return sorted(options, key=lambda item: item["name"])
+
+
+def ctx_options_payee(query: orm.Query) -> t.List[t.DictStr]:
+  """Get the context to build the options for payee
+
+  Args:
+    query: Query to use to get distinct values
+
+  Returns:
+    List of HTML context
+  """
+  # Get options with these filters
+  options: t.List[t.DictStr] = []
+  for payee, in query.with_entities(TransactionSplit.payee).distinct():
+    options.append({"name": payee})
+  return sorted(options, key=lambda item: item["name"])
+
+
+def ctx_options_category(query: orm.Query,
+                         categories: t.DictIntStr) -> t.List[t.DictStr]:
+  """Get the context to build the options for category
+
+  Args:
+    query: Query to use to get distinct values
+    categories: Category ID to name mapping
+
+  Returns:
+    List of HTML context
+  """
+  # Get options with these filters
+  options: t.List[t.DictStr] = []
+  for cat_id, in query.with_entities(TransactionSplit.category_id).distinct():
+    options.append({"name": categories[cat_id]})
+  return sorted(options, key=lambda item: item["name"])
+
+
+def ctx_options_tag(query: orm.Query) -> t.List[t.DictStr]:
+  """Get the context to build the options for tag
+
+  Args:
+    query: Query to use to get distinct values
+
+  Returns:
+    List of HTML context
+  """
+  # Get options with these filters
+  options: t.List[t.DictStr] = []
+  for tag, in query.with_entities(TransactionSplit.tag).distinct():
+    if tag is None:
+      continue
+    options.append({"name": tag})
+  # TODO Add checked or not
+  # TODO Sort by name and checked on top
+  return sorted(options, key=lambda item: item["name"])
 
 
 def ctx_table() -> t.DictAny:
@@ -72,13 +146,20 @@ def ctx_table() -> t.DictAny:
     query = query.where(TransactionSplit.asset_id.is_(None))
     if start is not None:
       query = query.where(TransactionSplit.date >= start)
-    if locked is not None:
-      query = query.where(TransactionSplit.locked == locked)
     query = query.where(TransactionSplit.date <= end)
     query = query.order_by(TransactionSplit.date)
 
-    # TODO (WattsUp) Add Account, Payee, Category, and Tag filters
-    # With search on drop downs
+    # Get options with these filters
+    options_account = ctx_options_account(query, accounts)
+    options_payee = ctx_options_payee(query)
+    options_category = ctx_options_category(query, categories)
+    options_tag = ctx_options_tag(query)
+
+    # TODO Make options filter transactions
+    # TODO Add searching
+
+    if locked is not None:
+      query = query.where(TransactionSplit.locked == locked)
 
     if search_str != "":
       query = search(query, TransactionSplit, search_str)
@@ -133,7 +214,11 @@ def ctx_table() -> t.DictAny:
         "end": end,
         "period": period,
         "search": search_str,
-        "locked": locked
+        "locked": locked,
+        "options-account": options_account,
+        "options-payee": options_payee,
+        "options-category": options_category,
+        "options-tag": options_tag,
     }
 
 
