2d4cc98edfc7d9e070ac38ccb94dd32c83da518f
Bradley Davis <me@bradleydavis.tech>
2023-12-15 21:03:48 -0800

    Added target balance to chart

--- nummus/static/src/emergency-fund/chart.js
+++ nummus/static/src/emergency-fund/chart.js
@@ -10,14 +10,26 @@ const emergencyFundChart = {
         'use strict';
         const dates = raw.dates;
         const values = raw.balances.map(v => Number(v));
+        const targetLow = Number(raw.target_low);
+        const targetHigh = Number(raw.target_high);
 
+        const green = getThemeColor('green');
 
         const canvas = document.getElementById('e-fund-chart-canvas');
         const ctx = canvas.getContext('2d');
         if (ctx == this.ctx) {
             chartSingle.update(this.chart, dates, values);
         } else {
-            const plugins = [];
+            const plugins = [
+                [
+                    pluginBoxAnnotation, {
+                        yMin: targetLow,
+                        yMax: targetHigh,
+                        borderWidth: 0,
+                        backgroundColor: green + '80',
+                    }
+                ],
+            ];
             this.ctx = ctx;
             this.chart = chartSingle.create(
                 ctx,
