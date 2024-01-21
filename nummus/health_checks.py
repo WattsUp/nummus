0612e9aa689a4210a281b7a84ea6c960e010e828
Bradley Davis <me@bradleydavis.tech>
2024-01-20 23:30:55 -0800

    Added MissingAssetValuations

--- nummus/health_checks.py
+++ nummus/health_checks.py
@@ -13,7 +13,14 @@ from typing_extensions import override
 
 from nummus import exceptions as exc
 from nummus import utils
-from nummus.models import Account, TransactionCategory, TransactionSplit, YIELD_PER
+from nummus.models import (
+    Account,
+    Asset,
+    AssetValuation,
+    TransactionCategory,
+    TransactionSplit,
+    YIELD_PER,
+)
 
 if TYPE_CHECKING:
     from nummus import custom_types as t
@@ -195,8 +202,42 @@ class MissingAssetValuations(Base):
 
     @override
     def test(self, p: portfolio.Portfolio) -> None:
-        self._issues = []
-        raise NotImplementedError
+        with p.get_session() as s:
+            assets = Asset.map_name(s)
+
+            query = (
+                s.query(TransactionSplit)
+                .with_entities(
+                    TransactionSplit.asset_id,
+                    sqlalchemy.func.min(TransactionSplit.date_ord),
+                )
+                .where(TransactionSplit.asset_id.isnot(None))
+                .group_by(TransactionSplit.asset_id)
+            )
+            first_date_ords: dict[int, int] = dict(query.yield_per(YIELD_PER))  # type: ignore[attr-defined]
+
+            query = (
+                s.query(AssetValuation)
+                .with_entities(
+                    AssetValuation.asset_id,
+                    sqlalchemy.func.min(AssetValuation.date_ord),
+                )
+                .group_by(AssetValuation.asset_id)
+            )
+            first_valuations: dict[int, int] = dict(query.yield_per(YIELD_PER))  # type: ignore[attr-defined]
+
+            for a_id, date_ord in first_date_ords.items():
+                date_ord_v = first_valuations.get(a_id)
+                if date_ord_v is None:
+                    msg = f"{assets[a_id]} has no valuations"
+                    self._issues.append(msg)
+                elif date_ord < date_ord_v:
+                    msg = (
+                        f"{assets[a_id]} has first transaction on"
+                        f" {datetime.date.fromordinal(date_ord)} before first valuation"
+                        f" on {datetime.date.fromordinal(date_ord_v)}"
+                    )
+                    self._issues.append(msg)
 
 
 class Typos(Base):
@@ -234,7 +275,9 @@ CHECKS: list[type[Base]] = [
     DatabaseIntegrity,
     UnbalancedTransfers,
     UnbalancedCreditCardPayments,
-    # MissingAssetValuations,
+    MissingAssetValuations,
     # Typos,
     # UnlockedTransactions,
+    # Bad asset splits if an asset is bought/sold for wildly different price
+    # Negative cash when buying assets, buying on margin needs a silence
 ]
