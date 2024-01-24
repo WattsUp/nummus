668847ff0f228c360fedb208842eaf8129a4f4c6
Bradley Davis <me@bradleydavis.tech>
2024-01-24 15:06:00 -0800

    Added Typos check

--- nummus/health_checks.py
+++ nummus/health_checks.py
@@ -3,11 +3,13 @@
 from __future__ import annotations
 
 import datetime
+import re
 import textwrap
 from abc import ABC, abstractmethod
 from decimal import Decimal
 from typing import TYPE_CHECKING
 
+import spellchecker
 import sqlalchemy
 from typing_extensions import override
 
@@ -329,10 +331,111 @@ class Typos(Base):
     _DESC = "Checks for very similar fields and common typos."
     _SEVERE = False
 
+    _RE_WORDS = re.compile(r"[ ,/]")
+
     @override
     def test(self, p: portfolio.Portfolio) -> None:
-        self._issues = []
-        raise NotImplementedError
+        spell = spellchecker.SpellChecker()
+        # Known correct words
+        known = {
+            "401k",
+            "etc.",
+            "Inc.",
+            "ETF",
+            "ATM",
+            "Uncategorized",
+            "Rebalance",
+        }
+        known = {k.lower() for k in known}
+        # TODO (WattsUp): Add mechanism to add to this known dictionary
+
+        with p.get_session() as s:
+            accounts = Account.map_name(s)
+
+            # Create a dict of every word found with the first instance detected
+            # Dictionary {word.lower(): (word, source, field)}
+            words: dict[str, tuple[str, str, str]] = {}
+
+            def add(s: str | None, source: str, field: str) -> None:
+                if s is None:
+                    return
+                for token in self._RE_WORDS.split(s):
+                    if not token:
+                        continue
+                    token_l = token.lower()
+                    if token_l not in words:
+                        words[token_l] = (token, source, field)
+
+            query = s.query(Account).with_entities(
+                Account.id_,
+                Account.institution,
+            )
+            for acct_id, institution in query.yield_per(YIELD_PER):
+                acct_id: int
+                institution: str
+                name = accounts[acct_id]
+                source = f"Account {name}"
+                add(name, source, "name")
+                add(institution, source, "institution")
+
+            query = s.query(Asset).with_entities(
+                Asset.name,
+                Asset.description,
+            )
+            for name, description in query.yield_per(YIELD_PER):
+                name: str
+                description: str
+                source = f"Asset {name}"
+                add(name, source, "name")
+                add(description, source, "description")
+
+            txn_fields = [
+                TransactionSplit.payee,
+                TransactionSplit.description,
+                TransactionSplit.tag,
+            ]
+            for field in txn_fields:
+                query = (
+                    s.query(TransactionSplit)
+                    .with_entities(
+                        TransactionSplit.date_ord,
+                        TransactionSplit.account_id,
+                        field,
+                    )
+                    .group_by(field)
+                )
+                for date_ord, acct_id, value in query.yield_per(YIELD_PER):
+                    date_ord: int
+                    acct_id: int
+                    value: str
+                    date = datetime.date.fromordinal(date_ord)
+                    source = f"{date} - {accounts[acct_id]}"
+                    add(value, source, field.key)
+
+            query = s.query(TransactionCategory.name)
+            for (name,) in query.yield_per(YIELD_PER):
+                name: str
+                add(name, f"Txn category {name}", "name")
+
+            words = {k: v for k, v in words.items() if k not in known}
+            words = {
+                k: v for k, v in words.items() if k.lower() in spell.unknown(words)
+            }
+
+            word_len = 0
+            source_len = 0
+            field_len = 0
+
+            for word, source, field in words.values():
+                word_len = max(word_len, len(word))
+                source_len = max(source_len, len(source))
+                field_len = max(field_len, len(field))
+
+            for word, source, field in words.values():
+                # Getting a suggested correction is slow and error prone,
+                # Just say if a word is outside of the dictionary
+                msg = f"{source:{source_len}} {field:{field_len}}: {word:{word_len}}"
+                self.issues.append(msg)
 
 
 class UnlockedTransactions(Base):
@@ -381,10 +484,11 @@ CHECKS: list[type[Base]] = [
     UnbalancedCreditCardPayments,
     MissingAssetValuations,
     OutlierAssetPrice,
-    # Typos,
+    Typos,
     UnlockedTransactions,
     # Negative cash when buying assets, buying on margin needs a silence
     # Dividends and investment fees not assigned to an asset
     # Interest assigned to an asset
     # null payee or description or uncategorized
+    # Duplicate transactions
 ]
