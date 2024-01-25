6536d526eba5654c8c7b103736dc34a1f3d3782e
Bradley Davis <me@bradleydavis.tech>
2024-01-25 11:34:03 -0800

    Added DuplicateTransactions

--- nummus/health_checks.py
+++ nummus/health_checks.py
@@ -19,6 +19,7 @@ from nummus.models import (
     Account,
     Asset,
     AssetValuation,
+    Transaction,
     TransactionCategory,
     TransactionSplit,
     YIELD_PER,
@@ -422,6 +423,9 @@ class Typos(Base):
                 k: v for k, v in words.items() if k.lower() in spell.unknown(words)
             }
 
+            if len(words) == 0:
+                return
+
             word_len = 0
             source_len = 0
             field_len = 0
@@ -544,24 +548,74 @@ class EmptyFields(Base):
                 source = f"{date} - {accounts[acct_id]}"
                 issues.append((source, "is uncategorized"))
 
+            if len(issues) == 0:
+                return
+
             source_len = max(len(item[0]) for item in issues)
             for source, field in issues:
                 msg = f"{source:{source_len}} {field}"
                 self._issues.append(msg)
 
 
+class DuplicateTransactions(Base):
+    """Checks for transactions with same amount and date."""
+
+    _NAME = "Duplicate Transactions"
+    _DESC = "Checks for transactions with same amount and date."
+    _SEVERE = True
+
+    @override
+    def test(self, p: portfolio.Portfolio) -> None:
+        with p.get_session() as s:
+            accounts = Account.map_name(s)
+
+            issues: list[tuple[str, str]] = []
+
+            query = (
+                s.query(Transaction)
+                .with_entities(
+                    Transaction.date_ord,
+                    Transaction.account_id,
+                    Transaction.amount,
+                )
+                .group_by(
+                    Transaction.date_ord,
+                    Transaction.account_id,
+                    Transaction.amount,
+                )
+                .having(sqlalchemy.func.count(Transaction.id_) > 1)
+            )
+            for date_ord, acct_id, amount in query.yield_per(YIELD_PER):
+                date_ord: int
+                acct_id: int
+                amount: t.Real
+                date = datetime.date.fromordinal(date_ord)
+                source = f"{date} - {accounts[acct_id]}"
+                issues.append((source, utils.format_financial(amount)))
+
+            if len(issues) == 0:
+                return
+            source_len = max(len(item[0]) for item in issues)
+            amount_len = max(len(item[1]) for item in issues)
+
+            for source, amount_str in issues:
+                msg = f"{source:{source_len}} {amount_str:>{amount_len}}"
+                self._issues.append(msg)
+
+
 # List of all checks to test
 CHECKS: list[type[Base]] = [
     DatabaseIntegrity,
+    DuplicateTransactions,
     UnbalancedTransfers,
     UnbalancedCreditCardPayments,
     MissingAssetValuations,
     OutlierAssetPrice,
+    # Not severe below
     Typos,
     UnlockedTransactions,
     EmptyFields,
     # Negative cash when buying assets, buying on margin needs a silence
     # Dividends and investment fees not assigned to an asset
     # Interest assigned to an asset
-    # Duplicate transactions
 ]
