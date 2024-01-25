c39bbe7c6c749ed54c70f45b018b920167eea3d0
Bradley Davis <me@bradleydavis.tech>
2024-01-25 12:03:05 -0800

    Added OverdrawnAccounts

--- nummus/health_checks.py
+++ nummus/health_checks.py
@@ -17,6 +17,7 @@ from nummus import exceptions as exc
 from nummus import utils
 from nummus.models import (
     Account,
+    AccountCategory,
     Asset,
     AssetValuation,
     Transaction,
@@ -603,6 +604,86 @@ class DuplicateTransactions(Base):
                 self._issues.append(msg)
 
 
+class OverdrawnAccounts(Base):
+    """Checks for accounts that had a negative cash balance when they shouldn't."""
+
+    _NAME = "Overdrawn Accounts"
+    _DESC = "Checks for accounts that had a negative cash balance when they shouldn't"
+    _SEVERE = True
+
+    @override
+    def test(self, p: portfolio.Portfolio) -> None:
+        today = datetime.date.today()
+        today_ord = today.toordinal()
+        with p.get_session() as s:
+            # Get a list of accounts subject to overdrawn so not credit and loans
+            categories = [
+                AccountCategory.CREDIT,
+                AccountCategory.LOAN,
+                AccountCategory.MORTGAGE,
+            ]
+            query = (
+                s.query(Account)
+                .with_entities(Account.id_, Account.name)
+                .where(Account.category.not_in(categories))
+            )
+            accounts: t.DictIntStr = dict(query.all())  # type: ignore[attr-defined]
+            acct_ids = set(accounts)
+
+            issues: list[tuple[str, str]] = []
+
+            start_ord = (
+                s.query(sqlalchemy.func.min(TransactionSplit.date_ord))
+                .where(TransactionSplit.account_id.in_(acct_ids))
+                .scalar()
+            )
+            if start_ord is None:
+                # No asset transactions at all
+                return
+            n = today_ord - start_ord + 1
+
+            for acct_id, name in accounts.items():
+                # Get cash holdings across all time
+                cash_flow = [None] * n
+                query = (
+                    s.query(TransactionSplit)
+                    .with_entities(
+                        TransactionSplit.date_ord,
+                        TransactionSplit.amount,
+                    )
+                    .where(
+                        TransactionSplit.account_id == acct_id,
+                    )
+                )
+                for date_ord, amount in query.yield_per(YIELD_PER):
+                    date_ord: int
+                    amount: Decimal
+
+                    i = date_ord - start_ord
+
+                    v = cash_flow[i]
+                    cash_flow[i] = amount if v is None else v + amount
+
+                cash = Decimal(0)
+                for i, c in enumerate(cash_flow):
+                    if c is None:
+                        continue
+                    cash += c
+                    if cash < 0:
+                        date = datetime.date.fromordinal(start_ord + i)
+                        source = f"{date} - {name}"
+                        issues.append((source, utils.format_financial(c)))
+
+            if len(issues) == 0:
+                return
+            source_len = max(len(item[0]) for item in issues)
+            amount_len = max(len(item[1]) for item in issues)
+
+            for source, amount_str in issues:
+                msg = f"{source:{source_len}} {amount_str:>{amount_len}}"
+                self._issues.append(msg)
+
+
 # List of all checks to test
 CHECKS: list[type[Base]] = [
     DatabaseIntegrity,
@@ -611,11 +692,11 @@ CHECKS: list[type[Base]] = [
     UnbalancedCreditCardPayments,
     MissingAssetValuations,
     OutlierAssetPrice,
+    OverdrawnAccounts,
     # Not severe below
     Typos,
     UnlockedTransactions,
     EmptyFields,
-    # Negative cash when buying assets, buying on margin needs a silence
     # Dividends and investment fees not assigned to an asset
     # Interest assigned to an asset
 ]
