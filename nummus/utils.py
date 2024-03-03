d5c54af1f93fd771f46d2fd6e45213ca21540f3f
Bradley Davis <me@bradleydavis.tech>
2024-03-02 17:38:43 -0800

    Changed mwrr to just use brentq

--- nummus/utils.py
+++ nummus/utils.py
@@ -8,7 +8,6 @@ import getpass
 import re
 import shutil
 import sys
-import warnings
 from decimal import Decimal
 
 from scipy import optimize
@@ -577,14 +576,7 @@ def mwrr(values: list[Decimal], profit: list[Decimal]) -> Decimal:
             return float("inf")
         return sum((cf / r ** (i / DAYS_IN_YEAR) for i, cf in cfs.items()))
 
-    try:
-        # Could try to go to very large rates but will fail to converge
-        # Ignore them and let brentq fix the failed convergence
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            result = optimize.newton(lambda r: xnpv(r, cash_flows), 1.0)
-    except RuntimeError:
-        result = optimize.brentq(lambda r: xnpv(r, cash_flows), 0.0, 1e10)
+    result = optimize.brentq(lambda r: xnpv(r, cash_flows), 0.0, 1e10)
     if not isinstance(result, float):
         msg = f"Optimize result was {type(result)} not float"
         raise TypeError(msg)
