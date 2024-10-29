38a05f2702651a0e178c8d52843f881e1b4e9efb
Bradley Davis <me@bradleydavis.tech>
2024-10-28 20:10:21 -0700

    Changed sidebar to toggle or pinned on lg

--- nummus/static/src/nav.js
+++ nummus/static/src/nav.js
@@ -26,6 +26,34 @@ const navigation = {
             });
         }
     },
+    /**
+     * On click, open accounts sidebar
+     */
+    openSidebar: function() {
+        document.querySelector('#sidebar').classList.toggle('open');
+    },
+    /**
+     * On click of outside of sidebar, close sidebar
+     *
+     * @param {Event} event Triggering event
+     */
+    closeSidebar: function(event) {
+        if (!event ||
+            !event.target.matches(
+                '#sidebar, #sidebar *, .sidebar-opener, .sidebar-opener *')) {
+            document.querySelector('#sidebar').classList.remove('open');
+        }
+    },
+    /**
+     * After sidebar swap has settled, reopen sidebar
+     *
+     * @param {Event} event Triggering event
+     */
+    preserveOpen: function(event) {
+        // Call is only on button on open sidebar, so add open back
+        document.querySelector('#sidebar').classList.add('open');
+    },
 };
 
 window.addEventListener('click', navigation.closeDropdown);
+window.addEventListener('click', navigation.closeSidebar);
