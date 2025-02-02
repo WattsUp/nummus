9231f5dd164d105bf04c32c2ce68bd978e37c43e
Bradley Davis <me@bradleydavis.tech>
2025-02-02 15:09:54 -0800

    Added drawer opener and scrim

--- nummus/static/src/nav.js
+++ nummus/static/src/nav.js
@@ -1,79 +1,22 @@
 'use strict';
-const navigation = {
+const nav = {
     /**
-     * On click of a nav-folder title, toggle open
-     *
-     * @param {Element} src Triggering element
+     * On click, open nav-drawer
      */
-    openDropdown: function(src) {
-        let navFolder = src.parentNode;
-        document.querySelectorAll('.nav-folder').forEach((e) => {
-            if (e != navFolder) {
-                e.classList.remove('open');
-            } else {
-                e.classList.toggle('open');
-            }
-        });
+    openDrawer: function() {
+        htmx.addClass(htmx.find('#nav-drawer'), 'open');
     },
     /**
-     * On click of a nav-link, close all nav-folders
+     * On click of outside of nav-drawer, close nav-drawer
      *
      * @param {Event} event Triggering event
      */
-    closeDropdown: function(event) {
-        if (!event || !event.target.matches('.nav-folder, .nav-folder *')) {
-            document.querySelectorAll('.nav-folder').forEach((e) => {
-                e.classList.remove('open');
-            });
-        }
-    },
-    /**
-     * On click of a txn-filter icon, toggle open
-     *
-     * @param {Element} src Triggering element
-     */
-    openTxnFilter: function(src) {
-        let txnFilter = src.parentNode;
-        document.querySelectorAll('.txn-filter').forEach((e) => {
-            if (e != txnFilter) {
-                e.classList.remove('open');
-            } else {
-                e.classList.toggle('open');
-            }
-        });
-    },
-    /**
-     * On click outside a txn-filter, close all txn-filters
-     *
-     * @param {Event} event Triggering event
-     */
-    closeTxnFilter: function(event) {
-        if (!event || !event.target.matches('.txn-filter, .txn-filter *')) {
-            document.querySelectorAll('.txn-filter').forEach((e) => {
-                e.classList.remove('open');
-            });
-        }
-    },
-    /**
-     * On click, open accounts sidebar
-     */
-    openSidebar: function() {
-        document.querySelector('#sidebar').classList.toggle('open');
-    },
-    /**
-     * On click of outside of sidebar, close sidebar
-     *
-     * @param {Event} event Triggering event
-     */
-    closeSidebar: function(event) {
-        if (!event ||
-            !event.target.matches(
-                '#sidebar, #sidebar *, .sidebar-opener, .sidebar-opener *')) {
-            document.querySelector('#sidebar').classList.remove('open');
+    closeDrawer: function(event) {
+        const query = '#nav-drawer, #nav-drawer *, .nav-opener, .nav-opener *';
+        if (!event || !event.target.matches(query)) {
+            htmx.removeClass(htmx.find('#nav-drawer'), 'open');
         }
     },
 };
 
-window.addEventListener('click', navigation.closeDropdown);
-window.addEventListener('click', navigation.closeSidebar);
-window.addEventListener('click', navigation.closeTxnFilter);
+window.addEventListener('click', nav.closeDrawer);
